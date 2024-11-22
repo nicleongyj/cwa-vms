1ef1fd77bceba54361a0114404ad031175481fe6
Yong Jing <39022090+gohyongjing@users.noreply.github.com>
2024-11-22 08:33:09 +0000

    Add fail-fast code for missing credentials

--- src/lib/supabase/index.ts
+++ src/lib/supabase/index.ts
@@ -4,6 +4,14 @@ import { createClient } from "@supabase/supabase-js";
 const supabase_url = process.env.SUPABASE_URL ?? "";
 const supabase_key = process.env.SUPABASE_KEY ?? "";
 
+if (supabase_url === undefined) {
+  throw new Error(`Env variable SUPABASE_URL undefined`);
+}
+
+if (supabase_key === undefined) {
+  throw new Error(`Env variable SUPABASE_KEY undefined`);
+}
+
 const supabase = createClient(supabase_url, supabase_key, {});
 
 export { supabase };
