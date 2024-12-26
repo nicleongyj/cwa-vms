7cd6051c8704b57a110ee8aab9334ccc4c03e17f
nicleongyj <e0968256@u.nus.edu>
2024-12-26 18:09:49 +0800

    feat: add donor route

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import userRouter from "./user/userRouter";
 import authRouter from "./auth/authRouter";
+import donorRouter from "./donor/donorRouter";
 
 export default () => {
     const app = Router();
     authRouter(app);
     userRouter(app);
+    donorRouter(app);
+
 
     return app;
 };
