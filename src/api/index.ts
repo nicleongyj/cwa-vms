36cc284316f4c20fa36a35032195d287ab487c42
Yong Jing <39022090+gohyongjing@users.noreply.github.com>
2025-01-02 14:53:49 +0000

    fix: remove unused auth router

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import userRouter from "./user/userRouter";
-import authRouter from "./auth/authRouter";
+// import authRouter from "./auth/authRouter";
 import devRouter from "./dev/devRouter";
 
 export default () => {
     const app = Router();
-    authRouter(app);
+    // authRouter(app);
     userRouter(app);
     devRouter(app);
 
